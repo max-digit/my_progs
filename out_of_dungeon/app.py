436a4387cf005d1780eb078eb148b44f26da5bd6
Maxim <maxharley@mail.ru>
2022-11-22 20:54:22 +0500

    Fixed logic, session added

--- out_of_dungeon/app.py
+++ out_of_dungeon/app.py
@@ -1,27 +1,27 @@
-from flask import Flask, request, render_template, redirect, url_for
+import secrets
+from flask import Flask, request, render_template, redirect, session, url_for
 
 from out_of_dungeon import GameForm, Player, Castle
-from config import Config
 
 app = Flask(__name__)
-app.config.from_object(Config())
+app.secret_key =  secrets.token_hex()
 
-
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
+    if 'username' in session:
+        return f'Logged in as {session["username"]}'
+    # return 'You are not logged in'
+    return redirect(url_for('login'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
     form = GameForm()
-    Player.name = form.player_name.data
-    Castle(floor=0, room=0)
     if request.method == 'POST':
-        if form.validate_on_submit:
-            return redirect(
-                url_for(
-                    'game',
-                    name=Player.name,
-                    way=None,
-                    steps=None
-                    )
-                )
+        Player.name = form.player_name.data
+        session['username'] = Player.name
+        return redirect(
+            url_for('game', name=Player.name, way=None, steps=None)
+            )
     return render_template(
         'index.html',
         form = form
@@ -30,26 +30,42 @@ def index():
 @app.route('/game/<string:name>/', methods=['GET','POST'])
 @app.route('/game/<string:name>/<string:way>/<int:steps>/', methods=['GET','POST'])
 def game(name, way=None, steps=None):
-    form = GameForm()
-    Player.name = name
-    castle = Castle()
-    if request.method == 'GET':
-        if way is not None and steps is not None:
-            way = way
-            steps = steps
-    elif request.method == 'POST':
-        way = form.way.data
-        steps = form.steps.data
-    walk = castle.move(way, steps)
-    return render_template(
-        'game.html',
-        form = form,
-        Player = Player,
-        castle = castle,
-        way = way,
-        steps = steps,
-        walk = walk,
-        )
+    if 'username' in session:
+        form = GameForm()
+        Player.name = name
+        if 'floor' and 'room' in session:
+            floor = session['floor']
+            room = session['room']
+            castle = Castle(floor, room)
+            if request.method == 'GET':
+                way = way
+                steps = steps
+            elif request.method == 'POST':
+                way = form.way.data
+                steps = form.steps.data
+            castle.move(way, steps)
+            session['floor'] = castle.floor
+            session['room'] = castle.room
+        else:
+            session['floor'] = 0
+            session['room'] = 0
+            castle = Castle()
+        return render_template(
+            'game.html',
+            form = form,
+            Player = Player,
+            castle = castle,
+            way = way,
+            steps = steps,
+            )
+    else:
+        return redirect(url_for('index'))
+
+@app.route('/logout')
+def logout():
+    # remove the username from the session if it's there
+    session.pop('username', None)
+    return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
--- out_of_dungeon/out_of_dungeon.py
+++ out_of_dungeon/out_of_dungeon.py
@@ -1,5 +1,4 @@
-from threading import Lock
-
+# from threading import Lock
 from flask_wtf import FlaskForm
 from wtforms import IntegerField, SelectField, StringField, SubmitField
 from wtforms.validators import InputRequired, Length, NumberRange
@@ -37,7 +36,6 @@ class GameForm(FlaskForm):
         # default = 1
     )
 
-
     month = SelectField(
         "Выберите месяц: ",
         validators=[InputRequired()],
@@ -60,7 +58,7 @@ class GameForm(FlaskForm):
     )
 
 
-class SingletonMeta(type):
+"""class SingletonMeta(type):
     _instances = {}
     _lock: Lock = Lock()
 
@@ -69,7 +67,7 @@ class SingletonMeta(type):
             if cls not in cls._instances or args or kwargs:
                 instance = super().__call__(*args, **kwargs)
                 cls._instances[cls] = instance
-        return cls._instances[cls]
+        return cls._instances[cls]"""
 
 
 class Player:
@@ -79,20 +77,21 @@ class Player:
         cls.name = name
 
 
-class Castle(Player, metaclass=SingletonMeta):
+class Castle(Player):
 
     def __init__(self, floor=0, room=0, ) -> None:
         if hasattr(Player, 'name'):
-            self.name = getattr(Player, 'name')
+            self.player_name = getattr(Player, 'name')
         else:
-            self.name = 'Сэр'
+            self.player_name = 'Сэр'
         self.map = self.castle_build()
-        self.size = len(self.map)
-        self.edge = self.size - 1
-        self.floor = floor
-        self.room = room
         self.start = self.map[0][0]
         self.finish = self.map[2][1]
+        self.floor = floor
+        self.room = room
+        self.position = self.map[self.floor][self.room]
+        self.size = len(self.map)
+        self.edge = self.size - 1
 
     def castle_build(self):
         return [
@@ -101,41 +100,91 @@ class Castle(Player, metaclass=SingletonMeta):
             ["", "Балкон", ""],
             ]
 
-    def move(self, way: str, steps: int):
+    def move(self, way, steps):
         ways = ("север", "восток", "юг", "запад")
         if way is not None and steps is not None:
             if way in ways:
                 if steps > 0:
-                    if way == "север":
-                        yield from self.walk_north(steps)
-                    elif way == "восток":
-                        yield from self.walk_east(steps)
-                    elif way == "юг":
-                        yield from self.walk_south(steps)
-                    elif way == "запад":
-                        yield from self.walk_west(steps)
+                    for _step in range(1, steps + 1):
+                        if way == "север":
+                            # yield from self.walk_north(steps)
+                            if self.floor < self.edge:
+                                self.floor += 1
+                                if self.pos():
+                                    # yield from self.pos()
+                                    continue
+                                else:
+                                    self.floor -= 1
+                                    # yield from self.pos()
+                                    break
+                            else:
+                                # yield from self.pos()
+                                break
+                        elif way == "восток":
+                            # yield from self.walk_east(steps)
+                            if self.room < self.edge:
+                                self.room += 1
+                                if self.pos():
+                                    # yield from self.pos()
+                                    continue
+                                else:
+                                    self.room -= 1
+                                    # yield from self.pos()
+                                    break
+                            else:
+                                # yield from self.pos()
+                                break
+                        elif way == "юг":
+                            # yield from self.walk_south(steps)
+                            if self.floor > 0:
+                                self.floor -= 1
+                                if self.pos():
+                                    # yield from self.pos()
+                                    continue
+                                else:
+                                    self.floor += 1
+                                    # yield from self.pos()
+                                    break
+                            else:
+                                # yield from self.pos()
+                                break
+                        elif way == "запад":
+                            # yield from self.walk_west(steps)
+                            if self.room > 0:
+                                self.room -= 1
+                                if self.pos():
+                                    # yield from self.pos()
+                                    continue
+                                else:
+                                    self.room += 1
+                                    # yield from self.pos()
+                                    break
+                            else:
+                                # yield from self.pos()
+                                break
+                    return self.pos()
                 elif steps == 0:
-                    yield self.get_message()
+                    return self.message()
             elif way not in ways:
-                yield self.get_warning(way)
+                return self.warning(way)
         elif way is None or steps is None:
-            yield self.get_message()
+            return self.message()
 
     def walk_north(self, steps):
         for _step in range(1, steps + 1):
             if self.floor < self.edge:
                 self.floor += 1
                 if self.pos():
-                    yield self.get_message()
+                    yield self.message()
                 else:
                     self.floor -= 1
-                    yield self.get_notice()
+                    yield self.notice()
                     break
             else:
-                yield self.get_notice()
+                yield self.notice()
                 break
             if self.pos() == self.finish:
-                yield self.get_congratulation()
+                yield self.congratulation()
                 break
 
     def walk_east(self, steps):
@@ -143,16 +192,16 @@ class Castle(Player, metaclass=SingletonMeta):
             if self.room < self.edge:
                 self.room += 1
                 if self.pos():
-                    yield self.get_message()
+                    yield self.message()
                 else:
                     self.room -= 1
-                    yield self.get_notice()
+                    yield self.notice()
                     break
             else:
-                yield self.get_notice()
+                yield self.notice()
                 break
             if self.pos() == self.finish:
-                yield self.get_congratulation()
+                yield self.congratulation()
                 break
 
     def walk_south(self, steps):
@@ -160,16 +209,16 @@ class Castle(Player, metaclass=SingletonMeta):
             if self.floor > 0:
                 self.floor -= 1
                 if self.pos():
-                    yield self.get_message()
+                    yield self.message()
                 else:
                     self.floor += 1
-                    yield self.get_notice()
+                    yield self.notice()
                     break
             else:
-                yield self.get_notice()
+                yield self.notice()
                 break
             if self.pos() == self.finish:
-                yield self.get_congratulation()
+                yield self.congratulation()
                 break
 
     def walk_west(self, steps):
@@ -177,29 +226,32 @@ class Castle(Player, metaclass=SingletonMeta):
             if self.room > 0:
                 self.room -= 1
                 if self.pos():
-                    yield self.get_message()
+                    yield self.message()
                 else:
                     self.room += 1
-                    yield self.get_notice()
+                    yield self.notice()
                     break
             else:
-                yield self.get_notice()
+                yield self.notice()
                 break
             if self.pos() == self.finish:
-                yield self.get_congratulation()
+                yield self.congratulation()
                 break
+
+    def coord(self):
+        return {'floor':self.floor, 'room':self.room}
 
     def pos(self):
         return self.map[self.floor][self.room]
 
-    def get_message(self):
+    def message(self):
         return f'Вы в комнате {self.pos()}'
 
-    def get_notice(self):
+    def notice(self):
         return f'Вы упёрлись в стену комнаты {self.pos()}'
     
-    def get_warning(self, way):
+    def warning(self, way):
         return f'Такой стороны света ({way}) не существует. Проверьте введенные данные'
 
-    def get_congratulation(self):
+    def congratulation(self):
         return f'Отлично! Вы выбрались на {self.finish}! Свежий воздух бодрит, а барон Мюнхгаузен приветствует Вас вкуснейшим завтраком!'
