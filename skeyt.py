b2fb40cd6c748434efac4d0f50ab9ec515ab5211
Maxim <49806931+max-digit@users.noreply.github.com>
2022-12-12 22:16:15 +0500

    Update skeyt.py

--- skeyt.py
+++ skeyt.py
@@ -1,4 +1,5 @@
 def skeyt(dictionary, value):
+"Function that returns a list of dictionary keys, if they match a given value "
     keys = []
     def seek(key):
         if dictionary[key] == value:
